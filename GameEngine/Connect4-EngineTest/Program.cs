3c423aa041145becb855e1445846545392b9bc24
Yotam Levit <yotam.levit@gmail.com>
2022-04-18 21:48:23 +0300

    Test for the engine

---
+++ GameEngine/Connect4-EngineTest/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using Connect4_Engine.src;
+
+namespace Connect4_EngineTest
+{
+    class Program
+    {
+
+        public static TokenType Fun(TokenType a)
+        {
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            //TokenType a = 0;
+            int a = 0;
+
+            TokenType b = Fun((TokenType)a);
+            Console.WriteLine(b);
+            Console.WriteLine((int)b);
+            /*
+            Board board = new Board(5, 6);
+
+            board.PrintBoard();
+
+            Console.WriteLine("----------------------------------------");
+
+            board.InsertToken(TokenType.Player1, 2);
+
+            board.PrintBoard();
+
+            Console.WriteLine("----------------------------------------");
+
+            board.InsertToken(TokenType.Player1, 2);
+
+            board.PrintBoard();
+
+            Console.WriteLine("----------------------------------------");
+
+            board.InsertToken(TokenType.Player1, 2);
+
+            board.PrintBoard();
+
+            Console.WriteLine("----------------------------------------");
+
+            board.InsertToken(TokenType.Player1, 2);
+
+            board.PrintBoard();
+
+            Console.WriteLine("----------------------------------------");
+
+            board.InsertToken(TokenType.Player1, 2);
+
+            board.PrintBoard();
+
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine(board.InsertToken(TokenType.Player1, 2));
+
+            board.PrintBoard();
+            */
+        }
+    }
+}
