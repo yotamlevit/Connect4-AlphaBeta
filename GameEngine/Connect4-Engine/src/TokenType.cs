c028176d513d0841b81627c649fa58a812255e4f
Yotam Levit <yotam.levit@gmail.com>
2022-04-18 21:47:35 +0300

    Create an enum for token types

---
+++ GameEngine/Connect4-Engine/src/TokenType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Connect4_Engine.src
+{
+    /// <summary>
+    /// This enum represent all the possibol token types that can be used in the game of connect 4.
+    /// </summary>
+    public enum TokenType
+    {
+        /// <summary>
+        /// Player 1 token type.
+        /// </summary>
+        Player1,
+        /// <summary>
+        /// Player 2 token type.
+        /// </summary>
+        Player2
+    }
+}
