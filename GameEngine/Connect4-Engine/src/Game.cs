b295db2a39f57e5d4f4a580f831b22d277ef7011
Yotam Levit <yotam.levit@gmail.com>
2022-04-22 14:00:36 +0300

    Added Game object

---
+++ GameEngine/Connect4-Engine/src/Game.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Connect4_Engine.src
+{
+    public class Game
+    {
+
+        private Board GameBoard;
+
+        private int TurnCount;
+
+        private int[] Moves;
+
+        /// <summary>
+        /// Init Function
+        /// </summary>
+        public Game()
+        {
+            this.GameBoard = new Board();
+            this.TurnCount = 0;
+            this.Moves = new int[42];    
+
+        }
+
+        public int GetPlayerMove()
+        {
+            Console.Write("Enter Column To Insert Token: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        public void Start()
+        {
+            bool IsWin = false;
+            int PlayerMove;
+            TokenType CurrentPlayer = TokenType.Player1;
+
+            while(!IsWin && this.GameBoard.AvailableMoves().Count != 0)
+            {
+                Console.Clear();
+                CurrentPlayer = (TokenType)(this.TurnCount % 2);
+                Console.WriteLine("Its " + CurrentPlayer + " Turn");
+
+                Console.WriteLine(this.GameBoard.ToString());
+
+                PlayerMove = GetPlayerMove();
+                if (!MakeMove(PlayerMove))
+                {
+                    Console.WriteLine("Errorrrr");
+                }
+
+                IsWin = this.GameBoard.CheckPlayerWin(CurrentPlayer);
+            }
+            Console.Clear();
+            Console.WriteLine(this.GameBoard.ToString());
+
+            if (this.GameBoard.AvailableMoves().Count == 0)
+            {
+                Console.WriteLine("Board if FULL its a TIE");
+            }
+            else
+            {
+                Console.WriteLine(CurrentPlayer + " WIN!!!!!!!!!");
+            }
+        }
+
+        public bool MakeMove(int InsertionColumne)
+        {
+            //Give token ype of player1 if the turn counter if even else player2
+            if (!this.GameBoard.InsertToken((TokenType)(this.TurnCount % 2), InsertionColumne))
+                return false;
+
+            this.Moves[this.TurnCount++] = InsertionColumne;
+            return true;
+        }
+
+        public bool UndoMove()
+        {
+            int Col = this.Moves[--this.TurnCount];
+            if (!this.GameBoard.RemoveToken((TokenType)(this.TurnCount % 2), Col))
+                return true;
+
+            this.TurnCount++;
+
+            return false;
+        }
+    }
+}
