f14adbae377ed08628f63129018ebb202f4b7a44
Yotam Levit <yotam.levit@gmail.com>
2022-06-04 18:27:02 +0300

    Fixed Negamax algorithem

--- GameEngine/Connect4-AlphaBeta/src/AlphaBeta.cs
+++ GameEngine/Connect4-AlphaBeta/src/AlphaBeta.cs
@@ -8,46 +8,83 @@ namespace Connect4_AlphaBeta
         private readonly int DepthDifficulty;
 
         private readonly TokenType AIPlayer;
+        private readonly TokenType OpponentPlayer;
 
 
         public AlphaBeta(TokenType AIPlayer, int DepthDifficulty)
         {
             this.AIPlayer = AIPlayer;
+            this.OpponentPlayer = (int)AIPlayer == 0 ? TokenType.Player2 : TokenType.Player1;
             this.DepthDifficulty = DepthDifficulty;
         }
 
-        public int AlphaBetaNextMove(Board GameBoard, TokenType Player, int Depth=0)
-        {
-            int NextMove;
-            Board NextMoveBoard;
-            TokenType NextPlayer;
 
-            //Depth limit
-            if (Depth == DepthDifficulty)
-                return 0; //Eval Board
+        private int EvaluationFunction(Board GameBoard)
+        {
+            //Win AI
+            if (GameBoard.CheckPlayerWin(this.AIPlayer, 4))
+                return int.MaxValue;
+
+            //Win Opponent
+            if (GameBoard.CheckPlayerWin(this.OpponentPlayer, 4))
+                return int.MinValue;
 
             //Draw
             if (GameBoard.AvailableMoves().Count == 0)
                 return 0;
 
-            //Win
-            if (GameBoard.CheckPlayerWin(Player))
-                return Player == AIPlayer ? int.MaxValue : int.MinValue;
+            return 1;
+        }
+
+
+        public int FindBestMove(Board GameBoard)
+        {
+            int bestMove = 0, bestScore = int.MinValue;
+            Board nextMoveBoard;
+
+            for (int col = 1; col <= Board.Columns; col++)
+            {
+                nextMoveBoard = GameBoard.DeepCopy();
+                nextMoveBoard.InsertToken(this.AIPlayer, col);
+                if (this.AlphaBetaNextMove(nextMoveBoard, this.OpponentPlayer, 5) > bestScore)
+                {
+                    bestMove = col;
+                    bestScore = this.AlphaBetaNextMove(GameBoard, this.AIPlayer, 5);
+                }
+            }
+
+            return bestMove;
+        }
+
+        private int AlphaBetaNextMove(Board GameBoard, TokenType player, int Depth=0)
+        {
+            int bestScore, boardScore;
+            Board nextMoveBoard;
+            TokenType nextPlayer;
+
+            // //Depth limit
+            // if (Depth == DepthDifficulty)
+            //     return 0; //Eval Board
+
+            boardScore = this.EvaluationFunction(GameBoard);
+
+            if (boardScore == int.MinValue || boardScore == int.MaxValue || boardScore == 0)
+                return boardScore;
 
             // Set Minimum Value for the moves (To get the max)
-            NextMove = int.MinValue;
+            bestScore = int.MinValue;
             // Set the next player for the next layer
-            NextPlayer = (int)AIPlayer == 0 ? TokenType.Player2 : TokenType.Player1;
+            nextPlayer = player == this.AIPlayer ? this.OpponentPlayer : this.AIPlayer;
 
             // Next moves
             for (int Col = 1; Col <= Board.Columns; Col++)
             {
-                NextMoveBoard = GameBoard.DeepCopy();
-                NextMoveBoard.InsertToken(Player, Col);
-                NextMove = Math.Max(NextMove, this.AlphaBetaNextMove(NextMoveBoard, NextPlayer, Depth++));
+                nextMoveBoard = GameBoard.DeepCopy();
+                nextMoveBoard.InsertToken(player, Col);
+                bestScore = Math.Max(bestScore, this.AlphaBetaNextMove(nextMoveBoard, nextPlayer, Depth++));
             }
 
-            return NextMove;
+            return bestScore;
         
         
         }
